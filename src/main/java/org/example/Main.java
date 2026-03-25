d1abd7a7511c32429e94694b23ebe48746e7ceb1
av5916-crypto <av5916@srmiist.edu.in>
2026-03-25 13:34:24 +0530

    Added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String input = sc.nextLine();
+
+        String name;
+
+
+        if (input.isEmpty()) {
+            name = "World";
+        } else {
+            name = input;
+        }
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
